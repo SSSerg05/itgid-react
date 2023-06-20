7489f4a7a677e170d5394d1f3edfac514aafd420
SSSerg05 <ssserg05@gmail.com>
2023-06-20 20:51:05 +0300

    fix start
    Signed-off-by: SSSerg05 <ssserg05@gmail.com>

--- unit_1/src/index.js
+++ unit_1/src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom'; // for ver 17.x.x
+import { createRoot } from 'react-dom/client'; // for ver 18.x.x
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// new for 18.x.x
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
+
+/// old for 17.x.x
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
